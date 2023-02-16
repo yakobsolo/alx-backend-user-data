d4f59aa533efff830f6c1fd87591bb8eb4c1ca4c
yakobsolo <yakisolo2009@gmail.com>
2023-02-16 18:51:17 +0000

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-"""  mapping declaration of SQLAlchemy """
-from sqlalchemy import Column, Integer, String
+""" User model """
 from sqlalchemy.ext.declarative import declarative_base
-
-
+from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
 class User(Base):
-    """ SQLAlchemy model named User for a database table named users """
+    """ SQLAlchemy model for a database table named users """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
